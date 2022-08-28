074585a3a15f249c0305e74a9f044467ca90ca9a
Refilwe-M-Mashile <=>
2022-08-28 17:18:40 +0200

    Can place Xs

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" 
+      onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
 class Board extends Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
